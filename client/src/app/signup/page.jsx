a90b712e722e571db264c135aa77622ad5e8750d
Mehakb77 <mehakb@winuxsoftwaresolutions.com>
2023-08-09 22:20:12 +0530

    [feature] create a signup page

--- client/src/app/signup/page.jsx
+++ client/src/app/signup/page.jsx
@@ -1,7 +1,45 @@
-import React from "react";
+"use client";
+
+import React, { useState } from "react";
+import { fields } from "./constant";
+import Input from "@/components/Input/Input";
+import Link from "next/link";
+import Button from "@/components/Button/Button";
+import style from "../page.module.css";
+import Seprator from "@/components/seprator/Seprator";
 
 const SignUp = (props) => {
-  return <div>page</div>;
+  const [info, setInfo] = useState({
+    email: "",
+    password: "",
+    confirmPassword: "",
+  });
+
+  const onChange = (name, value) => {
+    setInfo((prev) => ({ ...prev, [name]: value }));
+  };
+
+  return (
+    <div className={style.centered}>
+      <div className={style.card}>
+        {fields.map((item, index) => (
+          <Input
+            key={index}
+            {...item}
+            onChange={onChange}
+            value={info[item.name]}
+          />
+        ))}
+
+        <Button>Singup</Button>
+        <div className={style.info}>
+          Already have an account? <Link href={"/login"}> Login</Link>
+        </div>
+        <Seprator />
+        <Button> Continue as Guest</Button>
+      </div>
+    </div>
+  );
 };
 
 export default SignUp;
