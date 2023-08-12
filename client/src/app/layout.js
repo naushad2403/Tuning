6d2b5d32c8e47ecb210cfe7665a20fcab494fffb
Mehakb77 <b7mehak@gmail.com>
2023-08-12 15:51:23 +0530

    [feature]: added usermenu and tab nav bar

--- client/src/app/layout.js
+++ client/src/app/layout.js
@@ -1,3 +1,4 @@
+import TopNavBar from "@/layouts/TopNavBar";
 import "./globals.css";
 import { Inter } from "next/font/google";
 
@@ -11,7 +12,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <TopNavBar />
+        {children}
+      </body>
     </html>
   );
 }
