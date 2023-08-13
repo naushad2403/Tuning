c9d751ec4259be861fcdeaadd653aece66634fbf
Mehakb77 <b7mehak@gmail.com>
2023-08-13 13:51:31 +0530

    [feature]: signup api integration

--- client/next.config.js
+++ client/next.config.js
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {};
+module.exports = {
+  async rewrites() {
+    return [
+      {
+        source: "/user/:path*",
+        destination:
+          "https://6nwki6q3vl.execute-api.us-east-1.amazonaws.com/user/:path*",
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+// module.exports = nextConfig
